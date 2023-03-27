f1341ec79daba9d2b9fffad10018a00587952463
cereceda1991 <cereceda1991@gmail.com>
2023-03-27 08:27:17 -0500

    Api Products

---
+++ README.md
@@ -0,0 +1,23 @@
+<p align="center">
+  <img src="./images/Captura.PNG" alt="Ejemplo">
+</p>
+API de Productos
+License: MIT
+
+Bienvenido a la API de Productos. Esta API te permite realizar operaciones CRUD (Crear, Leer, Actualizar y Borrar) en una base de datos de productos. La API está alojada en Digital Ocean.
+
+Instalación
+Para utilizar esta API, necesitas tener instalado Node.js. Sigue los siguientes pasos para instalar la API en tu máquina local:
+
+Clona este repositorio en tu máquina local.
+Ejecuta npm install para instalar las dependencias.
+PORT=3000
+Ejecuta npm start:dev para iniciar la API.
+Uso
+Una vez que la API esté instalada y en ejecución, puedes utilizarla para realizar operaciones CRUD en la base de datos de productos. Para más detalles, consulta la documentación de la API.
+
+Contribuir
+Si deseas contribuir a este proyecto, por favor lee las pautas de contribución antes de hacerlo.
+
+Licencia
+Este proyecto está bajo la Licencia MIT. 
--- app.js
+++ app.js
@@ -1,79 +1,93 @@
-const express = require('express')
+const express = require("express");
 let productId = 1;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-
+app.use(express.json());
 
 const products = [];
 
 const findAllProducts = (req, res) => {
-    res.json(products);
+  res.json(products);
 };
 
 const createProduct = (req, res) => {
-    const { name, price, description } = req.body;
-    const newProduct = {
-        id: productId,
-        name,
-        price,
-        description
-    };
-    products.push(newProduct); // Agregamos el nuevo producto a la matriz de productos
-    productId++; // incrementamos productId para el próximo producto
-    res.status(201).json(newProduct);
-}
-
+  const { name, ingredients, quantity, price, category, description } =
+    req.body;
+  const newProduct = {
+    id: productId,
+    name,
+    ingredients,
+    quantity,
+    price,
+    category,
+    description,
+  };
+  products.push(newProduct); // Agregamos el nuevo producto a la matriz de productos
+  productId++; // incrementamos productId para el próximo producto
+  res.status(201).json(newProduct);
+};
 
 const updateProduct = (req, res) => {
-    const { id } = req.params;
-    const { name, price, description } = req.body;
+  const { id } = req.params;
+  const { name, ingredients, quantity, price, category, description } =
+    req.body;
 
-    // buscar el producto correspondiente en la matriz de productos
-    const product = products.find(p => p.id == id);
+  // buscar el producto correspondiente en la matriz de productos
+  const product = products.find((p) => p.id == id);
 
-    if (!product) {
-        return res.status(404).json({ message: `Producto con ID ${id} no encontrado` });
-    }
+  if (!product) {
+    return res
+      .status(404)
+      .json({ message: `Producto con ID ${id} no encontrado` });
+  }
 
-    // actualizar las propiedades relevantes del producto
-    if (name) {
-        product.name = name;
-    }
-    if (price) {
-        product.price = price;
-    }
-    if (description) {
-        product.description = description;
-    }
+  // actualizar las propiedades relevantes del producto
+  if (name) {
+    product.name = name;
+  }
+  if (ingredients) {
+    product.ingredients = ingredients;
+  }
+  if (quantity) {
+    product.quantity = quantity;
+  }
+  if (price) {
+    product.price = price;
+  }
+  if (category) {
+    product.category = category;
+  }
+  if (description) {
+    product.description = description;
+  }
 
-    res.json(product);
-}
-
-
+  res.json(product);
+};
 
 const deleteProduct = (req, res) => {
-    const { id } = req.params;
-    const index = products.findIndex((product) => product.id == id);
+  const { id } = req.params;
+  const index = products.findIndex((product) => product.id == id);
 
-    if (index !== -1) {
-        products.splice(index, 1);
-        res.sendStatus(204);
-    } else {
-        res.sendStatus(404);
-    }
+  if (index !== -1) {
+    products.splice(index, 1);
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
 };
 
 const findProductById = (req, res) => {
-    const { id } = req.params;
-    const product = products.find(p => p.id == id);
+  const { id } = req.params;
+  const product = products.find((p) => p.id == id);
 
-    if (!product) {
-        return res.status(404).json({ message: `Producto con ID ${id} no encontrado` });
-    }
+  if (!product) {
+    return res
+      .status(404)
+      .json({ message: `Producto con ID ${id} no encontrado` });
+  }
 
-    res.json(product);
+  res.json(product);
 };
 
 app.get("/api/v1/products", findAllProducts);
@@ -88,9 +102,5 @@ app.delete("/api/v1/products/:id", deleteProduct);
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-})
-
-
-
-
+  console.log(`App running on port ${port}...`);
+});
